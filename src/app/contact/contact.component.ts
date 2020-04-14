2b7997487e460c7a74293d1559f42d4e0b92d01b
Ariel Halevi <Arielh@one1.co.il>
2020-04-14 03:40:16 +0300

    more changes

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  myMail = 'Arriel12@gmail.com';
+  myLocation = 'Ets Efraim, Israel';
+  myPhone = '+972 509553255';
 
   constructor() { }
 
